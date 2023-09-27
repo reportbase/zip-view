3da1cf368d3c8f801012c62adbe881a046725520
Tom Brinkman <reportbase@gmail.com>
2023-09-26 22:35:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3672,7 +3672,7 @@ var buttonlst =
 		    var ww = Math.floor(rect.width);
 		 var hhh = hh;
 		var yyy = 0;
-		if (1)//user.rect.height-user.rect.y > window.innerHeight)
+		if (0)//user.rect.height-user.rect.y > window.innerHeight)
 		{
 			hhh = window.innerHeight-user.rect.y;
 			if (user.rect.y < 0)
