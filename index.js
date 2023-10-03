8d3798674886202431c70b9c994567a66f962c15
Tom Brinkman <reportbase@gmail.com>
2023-10-03 13:37:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5735,7 +5735,7 @@ else if (url.searchParams.has("data")) {
 		.then((obj) => galleryobj.init(obj))
 		.catch((error) => {});
 } else {
-	url.path = url.searchParams.get("res/reci.json");
+	url.path = url.searchParams.get("res/home.json");
 	fetch("res/reci.json")
 		.then(response => jsonhandler(response))
 		.then((obj) => galleryobj.init(obj))
