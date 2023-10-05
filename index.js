19457e3a53bd07752b556939ebe9c5b272eded5d
Tom Brinkman <reportbase@gmail.com>
2023-10-04 21:07:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5575,8 +5575,8 @@ galleryobj.init = function(obj)
 	var a = Array(_6cnv.sliceobj.length()).fill().map((_, index) => index);
 	_6cnv.rotated = [...a, ...a, ...a];
 
-	_7cnv.sliceobj.data = [
-
+	_7cnv.sliceobj.data = galleryobj.basemenu ? galleryobj.basemenu :
+		[
 		{
 			title: "Goto\nkey+g",
 			func: function() 
@@ -5587,29 +5587,11 @@ galleryobj.init = function(obj)
 				gotodialog(value, "Goto", goimage);
 			}
 		},
-
 		{
 			title: "File Explorer\nkey+x",
 			func: function() {
 				importdialog();
 			}
-		},
-		{
-			title: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
-   				accusantium doloremque laudantium, totam rem aperiam, eaque 
-       				ipsa quae ab illo inventore veritatis et quasi architecto 
-	   			beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem 
-       				quia voluptas sit aspernatur aut odit aut fugit, sed quia 
-	   			consequuntur magni dolores eos qui ratione voluptatem sequi 
-       				nesciunt. Neque porro quisquam est, qui dolorem ipsum quia 
-	   			dolor sit amet, consectetur, adipisci velit, sed quia non 
-       				numquam eius modi tempora incidunt ut labore et dolore magnam 
-	   			aliquam quaerat voluptatem. Ut enim ad minima veniam, quis 
-       				nostrum exercitationem ullam corporis suscipit laboriosam, 
-	   			nisi ut aliquid ex ea commodi consequatur? Quis autem 
-       				vel eum iure reprehenderit qui in ea voluptate velit esse 
-	   			quam nihil molestiae consequatur, vel illum qui dolorem 
-       				eum fugiat quo voluptas nulla pariatur?`
 		},
 		{
 			title: "Tom Brinkman\nAll Rights Reserved",
