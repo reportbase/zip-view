0d2d4e9542ba9e6fa4b91a7a5496d033ff01cd0e
Tom Brinkman <reportbase@gmail.com>
2023-10-07 07:04:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,39 @@ Copyright 2017 Tom Brinkman
 https://zip-view.com
 https://ipfs-view.com
 */
-  
+
+function oauthSignIn() 
+{
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': '866271378749-uupeiu6kqu3huchf701akl91p0tdaijr.apps.googleusercontent.com',
+                'redirect_uri': 'https://zip-view.com',
+                'response_type': 'token',
+                'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}
+
 function iOS() 
 {
 	return [
@@ -2595,6 +2627,8 @@ var keylst = [{
 			} else if (key == "f") {
 				evt.preventDefault();
 				screenfull.toggle();
+			}e lse if (key == "z") {
+				oauthSignIn()
 			}
 		}
 	},
