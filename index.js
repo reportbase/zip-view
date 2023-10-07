b6e6639d6b7758904e5c3ceab38030f859404dcf
Tom Brinkman <reportbase@gmail.com>
2023-10-07 08:49:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2621,9 +2621,7 @@ var keylst = [{
 			} else if (key == "f") {
 				evt.preventDefault();
 				screenfull.toggle();
-			} else if (key == "z") {
-				oauthSignIn()
-			}
+			} 
 		}
 	},
 	{
@@ -5551,6 +5549,23 @@ galleryobj.init = function(obj)
 			title: "Login",
 			func: function() 
 			{
+				oauthSignIn()
+			}
+		},
+		{
+			title: "Account",
+			func: function() 
+			{
+				if (auth2.isSignedIn.get()) 
+				{
+				  var profile = auth2.currentUser.get().getBasicProfile();
+				  console.log('ID: ' + profile.getId());
+				  console.log('Full Name: ' + profile.getName());
+				  console.log('Given Name: ' + profile.getGivenName());
+				  console.log('Family Name: ' + profile.getFamilyName());
+				  console.log('Image URL: ' + profile.getImageUrl());
+				  console.log('Email: ' + profile.getEmail());
+				}
 			}
 		},
 		{
