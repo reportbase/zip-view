c4efd4e393ee3b244c149ff0637646412a39f9bf
Tom Brinkman <reportbase@gmail.com>
2023-10-07 19:11:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5520,9 +5520,54 @@ galleryobj.init = function(obj)
 			func: function() {}
 		},
 		{
+			title: "Authenticate",
+			func: function() 
+			{
+			      var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+			        var form = document.createElement('form');
+			        form.setAttribute('method', 'GET'); 
+			        form.setAttribute('action', oauth2Endpoint);
+			        var params = {'client_id': '866271378749-uupeiu6kqu3huchf701akl91p0tdaijr.apps.googleusercontent.com',
+			                      'redirect_uri': 'https://zip-view.pages.com',
+			                      'response_type': 'token',
+			                      'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+			                      'include_granted_scopes': 'true',
+			                      'state': 'pass-through value'};
+			      
+			        for (var p in params) 
+				{
+			          var input = document.createElement('input');
+			          input.setAttribute('type', 'hidden');
+			          input.setAttribute('name', p);
+			          input.setAttribute('value', params[p]);
+			          form.appendChild(input);
+			        }         		
+			}
+		},
+		{
+			title: "Logout",
+			func: function() 
+			{
+			         auth2.signOut()
+			           .then(function(result)
+			            {
+			              console.log(result);
+			            })
+			           .catch(function(result)
+			           {
+			              console.log(result);
+			           })			
+			}
+		},
+		{
 			title: "Login",
 			func: function() 
 			{
+			    auth2.signIn().then(result => 
+		            {
+		              const profile = auth2.currentUser.get().profile;
+		              console.log(profile);
+		            });
 			}
 		},
 		{
