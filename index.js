2e10aa2668abfb20d5c099e7dd270e234ff4377e
Tom Brinkman <reportbase@gmail.com>
2023-10-20 16:32:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3903,7 +3903,7 @@ var buttonlst = [
         else if (user.enabled)
                 clr = MENUSELECT;
 
-        var a = new panel.cols([BUTTONMARGIN, 0, BUTTONMARGIN],
+        var a = new panel.rows([BUTTONMARGIN, 0, BUTTONMARGIN],
             [
                 0,
                 new panel.expand(new panel.rounded(clr, 4, SEARCHFRAME, 8, 8), canvas.buttonmargin, canvas.buttonmargin),
