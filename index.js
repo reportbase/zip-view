e33529848ec2d10e79095b0e666951c357281a7c
Tom Brinkman <reportbase@gmail.com>
2023-10-20 21:53:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4226,9 +4226,9 @@ menuobj.draw = function()
                 isvisiblecount += j.slice.isvisible ? 1 : 0;
                 if (slice.isvisible)
                     context.canvas.visibles.push(j);
-                context.translate(0, j.y);
+                ctx.translate(0, j.y);
                 context.canvas.draw(ctx, r, j.slice, j.n);
-                context.translate(0, -j.y);
+                ctx.translate(0, -j.y);
             }
         }
     }
