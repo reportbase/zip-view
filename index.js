a0017776838b83cb63cfa1d3e604b7717de90da3
Tom Brinkman <reportbase@gmail.com>
2023-10-29 08:22:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2010,7 +2010,7 @@ var wheelst =
         {
             context.canvas.pinching = 1;
             var k = delta < 0 ? 1 : -1;
-            buttonobj.addperc(k * 0.01);
+            buttonobj.addperc(k * 0.005);
             context.canvas.lastime = -0.0000000000101010101;
             menuobj.draw();
             context.canvas.pinching = 0;
