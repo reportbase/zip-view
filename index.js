926f5d2c4e1d9f647598ed0ebb86b6db0331f992
Tom Brinkman <reportbase@gmail.com>
2023-11-04 11:32:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2407,7 +2407,7 @@ var wheelst =
                 thumbimglst[n] = new Image();
             }                
 
-            var k = templatelst.length/100;
+            var k = templatelst.length/1000;
             var j = delta > 1 ? 1: -1;
             templateobj.add(j);
             menuobj.draw()
