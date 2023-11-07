ee28a882c7e4d3d8be8703ec644dfe214ac9415f
Tom Brinkman <reportbase@gmail.com>
2023-11-07 14:10:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1029,8 +1029,8 @@ var bossdisplaylst =
             var r = calculateAspectRatioFit(photo.image.width, photo.image.height, width, height);
             var h = r.height;
             var w = r.width;
-            var x = Math.floor(Math.nub(positxobj.value(), positxobj.length(), w, rect.width));
-            var y = Math.floor(Math.nub(posityobj.value(), posityobj.length(), h, rect.height));
+            var x = Math.floor(Math.nub(1, 2, w, rect.width));
+            var y = Math.floor(Math.nub(1, 2, h, rect.height));
             canvas.thumbrect = new rectangle(x, y, w, h);
             if (!headcnv.height || context.nothumb)
                 return;
