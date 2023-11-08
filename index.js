2a33d123499c5db50c0033089d1ea3b0e6dbe282
Tom Brinkman <reportbase@gmail.com>
2023-11-08 09:15:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1268,7 +1268,44 @@ var bossdisplaylst =
     title: "Details",
     draw: function(context, rect, user, time)
     {
-        //not editable
+        var str = `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur`;
+
+         var a = new panel.rowsA([80,40,0,data.length*25,0,40,80],
+        [
+            0,
+            0,
+            0,
+
+            new panel.cols([0,360,0],
+                [
+                    0,
+                    new wraptext(context, str, rect.width),
+                    0,
+                ])
+            
+            0,
+            new panel.cols([0,360,0],
+                [
+                    0,
+                    new panel.text(),
+                    0,
+                ]),
+            0,
+        ]);
+        
+        a.draw(context, rect, 
+               [
+                   0,
+                   0,
+                   0,
+                   
+                   str,
+
+                   0,
+                   "Update",
+                   0
+                ], 0)  
+        var a = new 
     }
 },
 {
