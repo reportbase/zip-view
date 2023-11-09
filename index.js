f19d7059452e162cb77f40e8aabf67df32245460
Tom Brinkman <reportbase@gmail.com>
2023-11-09 11:17:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1203,6 +1203,7 @@ var bossdisplaylst =
         context.editdescription = new rectangle();
         context.copydescription = new rectangle();
         context.pastedescription = new rectangle();
+        context.description = new rectangle();
         
         var str = `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur`;
         var j = 480;
@@ -1219,7 +1220,11 @@ var bossdisplaylst =
                 new panel.cols([0,j,0],
                 [
                     0,
-                    new panel.multitext(0, new panel.shadow(new panel.text())),
+                    new panel.layers(
+                    [
+                        new panel.rectangle(context.description),
+                        new panel.multitext(0, new panel.shadow(new panel.text())),
+                    ]),
                     0,
                 ]),
                 0,
@@ -4453,6 +4458,7 @@ bossobj.draw = function()
     delete context.delete;
     delete context.download;
     delete context.upload;
+    delete context.description;
     delete context.editdescription;
     delete context.copydescription;
     delete context.pastedescription;
