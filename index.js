4f4be5e2d9b5af8ba04d20bef692f045684b06fc
Tom Brinkman <reportbase@gmail.com>
2023-11-08 19:45:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6245,7 +6245,7 @@ function wraptext(ctx, text, maxWidth)
 let thumbfittedlst = [];
 let thumbimglst = [];
 
-async function imagepath(user)
+function imagepath(user)
 {
     var src;
     if (user.id && user.id.length >= 5 &&
