c11dd4fde8a5d9e435de263b7f9515bc7e000881
Tom Brinkman <reportbase@gmail.com>
2023-11-20 22:43:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4745,7 +4745,7 @@ menuobj.draw = function()
     
     var current = context.canvas.sliceobj.lerp(
         1 - context.canvas.timeobj.berp());
-    if (canvas.lastcurrent != current)
+    if (1)//canvas.lastcurrent != current)
     {
         canvas.lastcurrent = current;
         var size = Math.ceil(rect.height / canvas.buttonheight) + 4;
