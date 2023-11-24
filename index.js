42aa7cf01e910e28f6d70d79916bfd712aa45b6e
Tom Brinkman <reportbase@gmail.com>
2023-11-23 18:27:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6909,13 +6909,8 @@ menuobj.updown = function(context, delta)
   var canvas = context.canvas;
     canvas.autodirect = delta < 0 ? 1 : -1;
     var f = Math.abs(delta) / 100;
-    var b = canvas.reduce/100;
-    var g = Math.lerp(0.01, 2160, b);
-    var lst = [1.5, 1.75, 2.0, 2.25, 2.5, 3.0, 3.5, 4.0];
-    var j = util.clamp(0, 8, canvas.sliceobj.length());
-    var k = lst[j] * f;
-    canvas.slideshow = (sealobj.value() / canvas.virtualheight) * k * 1.5;
-    canvas.slidereduce = canvas.slideshow / g;
+    canvas.slideshow = (sealobj.value() / canvas.virtualheight) * f;
+    canvas.slidereduce = canvas.slideshow / 100;
 }
 
 function selectname(name)
