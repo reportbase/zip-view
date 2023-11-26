5060221e2f15d7c1a1e8a3cbd9a608822d90f0f0
Tom Brinkman <reportbase@gmail.com>
2023-11-25 22:15:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6965,7 +6965,7 @@ menuobj.updown = function(context, delta, divider)
     canvas.autodirect = delta < 0 ? 1 : -1;
     var k = Math.abs(delta)/20;
     canvas.slideshow = (sealobj.value() / canvas.virtualheight) * k;
-    canvas.slidereduce = canvas.slideshow / divider;
+    canvas.slidereduce = canvas.slideshow / delta;
 }
 
 galleryobj.leftright = function(context, delta)
