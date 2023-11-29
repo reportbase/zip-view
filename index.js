3e23f4eedfbc177dc4d9d216cf48d07e805092d6
Tom Brinkman <reportbase@gmail.com>
2023-11-29 16:05:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6758,9 +6758,9 @@ function setupmenus()
     var t = url.searchParams.get("t");
     var n = 0;
     for (; n < _9cnv.sliceobj.data.length; ++n)
-        if (t == _9cnv.sliceobj.data.data[n])
+        if (t == _9cnv.sliceobj.data[n])
             break;
-    if (n != _9cnv.sliceobj.data.length())
+    if (n != _9cnv.sliceobj.data.length)
         _9cnv.sliceobj.set(n);
         
     _2cnv.sliceobj.data = [];
