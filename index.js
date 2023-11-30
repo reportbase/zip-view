16ac2a98a03396d5e81893b4debd6237010f3c4a
Tom Brinkman <reportbase@gmail.com>
2023-11-29 19:26:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3695,7 +3695,7 @@ var taplst =
             headcnvctx.homemenurect && 
             headcnvctx.homemenurect.hitest(x, y))
         {
-            leftmenu(_3cnvctx)
+            rightmenu(_3cnvctx)
         }
         else if (
             headcnv.height &&
