028cbeeda161bcbf9586db727f6f5496fb23926a
Tom Brinkman <reportbase@gmail.com>
2023-12-02 18:39:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -531,8 +531,8 @@ panel.empty = function()
     this.draw = function(context, rect, user, time) {}
 };    
 
-var SEALDIVISOR = 10000;
-var SEAL = 62831.83
+var SEALDIVISOR = 100;
+var SEAL = 628.3183
 var sealobj = new circular_array("SEAL", SEAL);
 sealobj.set(SEAL/2);
 
