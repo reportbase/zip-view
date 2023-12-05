a262b57e26635179c8e7a7f8bfae5ceb0efd1c36
Tom Brinkman <reportbase@gmail.com>
2023-12-05 10:36:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6314,6 +6314,7 @@ function closemenu()
     var k = displaylst.findIndex(function(a){return a.name == "GALLERY"});
     displayobj.set(k);
     headobj.draw();
+	menuobj.show();
     //menuobj.draw();
 }
 
