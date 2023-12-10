8189318b26dce90558f258e9f00ac1ab9afc1ee6
Tom Brinkman <reportbase@gmail.com>
2023-12-10 10:34:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4972,7 +4972,7 @@ menuobj.draw = function(noclear)
         else
         {
             var t = time + (n * delayinterval);
-            var b = Math.tan(t);
+            var b = Math.tan(1.5*t);
             var j = Math.berp(-1, 1, b);
             var y = j * context.canvas.virtualheight;
             var e = (canvas.virtualheight - rect.height) / 2;
