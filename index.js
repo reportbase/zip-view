856fa2eb91eb3ee9fb0ac40c3562dd7105fad898
Tom Brinkman <reportbase@gmail.com>
2023-12-11 20:32:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6742,17 +6742,20 @@ function showusers()
 
 galleryobj.reset = function(obj)
 { 
-    var b = obj.data.length%24;
-    if (b >= 1 && b <= 7)
-    {
-        var b = obj.data.length;
-        var m = Math.floor(obj.data.length/24);
-        var a = m*24+8;
-        var size = a - b;
-        for (var n = 0; n < size; ++n)
-            galleryobj.data.push(galleryobj.data[0]);
-    }
-
+	if (obj && obj.data)
+	{
+	    var b = obj.data.length%24;
+	    if (b >= 1 && b <= 7)
+	    {
+	        var b = obj.data.length;
+	        var m = Math.floor(obj.data.length/24);
+	        var a = m*24+8;
+	        var size = a - b;
+	        for (var n = 0; n < size; ++n)
+	            galleryobj.data.push(galleryobj.data[0]);
+	    }
+	}
+	
     setfavicon();
     stretchobj.makerange("40-90", stretchobj.length());  
     stretchobj.set(90);
