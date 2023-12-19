85a5a7e6182c4a099c0a265200d4fb0d4420fac9
Tom Brinkman <reportbase@gmail.com>
2023-12-18 20:19:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -60,11 +60,11 @@ const MEDIUMFONT = "19px Archivo Black";
 const LARGEFONT = "21px Archivo Black";
 const HUGEFONT = "24px Archivo Black";
 */
-const SMALLFONT = "17px kanit";
-const DEFAULTFONT = "18px kanit";
-const MEDIUMFONT = "19px kanit";
-const LARGEFONT = "20px kanit";
-const HUGEFONT = "22px kanit";
+const SMALLFONT = "16px Source Code Pro";
+const DEFAULTFONT = "17px Source Code Pro";
+const MEDIUMFONT = "18px Source Code Pro";
+const LARGEFONT = "19px Source Code Pro";
+const HUGEFONT = "20px Source Code Pro";
 
 const SLICEWIDTH = 16;
 const ROTATEANCHORSIAE = 3;
