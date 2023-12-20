0096cc044da3a7a5a09cdf9c3a85e05e7d5081ff
Tom Brinkman <reportbase@gmail.com>
2023-12-20 11:17:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5233,6 +5233,7 @@ menuobj.draw = function()
         var j = {slice,x,y,n};
         slice.rect = new rectangle(0, j.y, rect.width, buttonheight);
         slice.isvisible = j.y > -buttonheight && j.y < window.innerHeight;
+        //todo broken
         if (j.slice.rect.hitest(window.innerWidth / 2, window.innerHeight / 2))
         {
             galleryobj.width = thumbimg.width;
