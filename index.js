dd59a82e78af8e11cbeb4279251e22e81972b8d5
Tom Brinkman <reportbase@gmail.com>
2023-12-29 13:39:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3043,7 +3043,7 @@ async function loadimages(blobs)
     local.button = 0;
     galleryobj.init(galleryobj)
     menuobj.draw();    
-    menuobj.updown(_8cnvctx, -120, 60)
+    menuobj.updown(_8cnvctx, -1, 1)
     _8cnvctx.swipetimeout = 
         setInterval(function(){menuobj.draw()}, GALLERYMAIN);
 }
