93b9746061793d1d965be4bcea32b36c86b9d48f
Tom Brinkman <reportbase@gmail.com>
2023-12-28 21:51:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1495,7 +1495,7 @@ var displaylst =
         var name = value.name;    
     	if (value.blob && value.blob.name)
     		name = value.blob.name;
-        name = cliptext(context, name, rainstep*0.4);
+        name = cliptext(context, name, rainstep*0.25);
         var a = new panel.rowsA(
         [
             HEADTOP, 
