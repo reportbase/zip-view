5191b2a493af4ff93f0f7cab09a5940a03374da0
Tom Brinkman <reportbase@gmail.com>
2023-12-29 09:19:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1870,8 +1870,8 @@ panel.fullscreen = function()
         var a = new panel.layers(
             [
                 new panel.rectangle(context.fullscreenrect),
-                document.fullscreenEnabled ?
-                new panel.shrink(new panel.circle(MENUTAP, TRANSPARENT, 4), CIRCLEIN, CIRCLEIN) : 0,
+                //document.fullscreenEnabled ?
+                //new panel.shrink(new panel.circle(MENUTAP, TRANSPARENT, 4), CIRCLEIN, CIRCLEIN) : 0,
                 new panel.shrink(new panel.circle(document.fullscreenEnabled ?
                     TRANSPARENT : SCROLLNAB, SEARCHFRAME, 4), CIRCLEOUT, CIRCLEOUT),
             ]);
