eb40c5c124542f5dd7ff2c5c920dd82fdaeb5f07
Tom Brinkman <reportbase@gmail.com>
2023-12-30 11:10:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3844,12 +3844,12 @@ function gotoimage(n)
 
 function aligntop() 
 {
-    var current = _8cnv.timeobj.current();
+    var anchor = _8cnv.timeobj.anchor();
     var k = 1-(window.innerHeight/buttonobj.value());
     var j = Math.PI/galleryobj.length();
     var e = j * k;
     _8cnv.timeobj.rotate(e/2)
-    if (current.toFixed(3) == _8cnv.timeobj.current().toFixed(3))
+    if (anchor.toFixed(3) == _8cnv.timeobj.current().toFixed(3))
     {
         var k = _8cnv.timeobj.length() / galleryobj.length();
         _8cnv.timeobj.rotate(k);
