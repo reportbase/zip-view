9b4b17d809a5a23de4840c81fba0eee3666bfef2
Tom Brinkman <reportbase@gmail.com>
2023-12-30 11:12:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3849,7 +3849,7 @@ function aligntop()
     var j = Math.PI/galleryobj.length();
     var e = j * k;
     _8cnv.timeobj.rotate(e/2)
-    if (anchor.toFixed(3) == _8cnv.timeobj.current().toFixed(3))
+    if (anchor.toFixed(3) == _8cnv.timeobj.anchor().toFixed(3))
     {
         var k = _8cnv.timeobj.length() / galleryobj.length();
         _8cnv.timeobj.rotate(k);
