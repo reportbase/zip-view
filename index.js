aa8d9780bfb680014402c1377ae7730c0139ccc9
Tom Brinkman <reportbase@gmail.com>
2023-12-31 08:06:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3577,15 +3577,9 @@ var keylst =
             {
 		        if (canvas.ctrlKey)    
         		{
-                    if (buttonobj.value() < window.innerHeight)
-                    {
-                        aligncenter(0)
-                    }
-                    else
-                    {
-                        aligncenter(0)
+                    aligncenter(0)
+                    if (buttonobj.value() > window.innerHeight)
                         aligntop();
-                    }
                     menuobj.draw();
                 }
                 else
@@ -4501,66 +4495,9 @@ var taplst =
             headobj.hide();
             menuobj.draw();
         }
-        else if (0)//x < rect.width/2)
-        {
-            if (buttonobj.value() < window.innerHeight)
-            {
-                aligncenter()
-                var k = _8cnv.timeobj.length() / galleryobj.length();
-                _8cnv.timeobj.rotate(k);
-                menuobj.draw();
-            }
-            else
-            {
-                var time = _8cnv.timeobj.current();
-                aligncenter()
-                aligntop(time);
-            }
-                
-            menuobj.draw();
-        }
-        else if (0)
-        {
-            if (buttonobj.value() < window.innerHeight)
-            {
-                aligncenter()
-                var k = _8cnv.timeobj.length() / galleryobj.length();
-                _8cnv.timeobj.rotate(-k);
-                menuobj.draw();
-            }
-            else
-            {
-                var time = _8cnv.timeobj.current();
-                aligncenter()
-                alignbottom(time);
-            }
-                
-            menuobj.draw();
-        }
         else
         {
-            var visibles = canvas.visibles;
-            var k;
-            for (k = 0; k < visibles.length; k++)
-            {
-                var j = visibles[k];
-                if (!j.slice || !j.slice.rect)
-                    continue;
-                if (j.slice.rect.hitest(x, y))
-                    break;
-            }
-
-            if (k == visibles.length)
-                return;
-            var n = visibles[k].n;
-            var nn = n+1;
-            var np = n-1;
-            if (nn >= galleryobj.length())
-                nn = 0;
-            if (nn == -1)
-                nn = galleryobj.length()-1;
-            var slice = canvas.sliceobj.data[n];
-            y -= slice.rect.y;
+            var n = getvisible(x, y);
             aligncenter(n);
             menuobj.draw();
         }
@@ -4756,6 +4693,24 @@ var taplst =
 
 var tapobj = new circular_array("TAP", taplst);
 
+function getvisible(x, y)
+{
+    var visibles = _8cnv.visibles;
+    var k;
+    for (k = 0; k < visibles.length; k++)
+    {
+        var j = visibles[k];
+        if (!j.slice || !j.slice.rect)
+            continue;
+        if (j.slice.rect.hitest(x, y))
+            break;
+    }
+
+    if (k == visibles.length)
+        return;
+    return visibles[k].n;
+}
+            
 Number.prototype.inrange = function(a, b)
 {
     var min = Math.min(a, b),
