f5fa25e13985649e2eca14f1555ab888c82bb606
Tom Brinkman <reportbase@gmail.com>
2024-01-01 13:32:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1656,7 +1656,7 @@ buttonobj.reset = function()
         return;
     var a = w / h;
     buttonobj.data = [];
-    var dheight = Math.floor(window.innerWidth / a);
+    var dheight = Math.floor(window.innerWidth / a) / 2;
     var bheight = h*5;
     var bwidth = bheight*a;
     while (bheight*bwidth > 4000*4000)
