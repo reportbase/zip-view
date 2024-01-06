90ddabed57a21414f1b18499db7ceb37edb45636
Tom Brinkman <reportbase@gmail.com>
2024-01-06 03:35:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1414,9 +1414,9 @@ var displaylst =
                         new panel.cols([0,0,0,0,0],
                         [
                             0,
-                            new panel.shrink(new panel.circle(),5,5),
-                            new panel.shrink(new panel.circle(),5,5),
-                            new panel.shrink(new panel.circle(),5,5),
+                            new panel.shrink(new panel.circle("white"),10,10),
+                            new panel.shrink(new panel.circle("white"),10,10),
+                            new panel.shrink(new panel.circle("white"),10,10),
                             0,
                         ])
                     ]),
