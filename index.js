5c940fbce99887147051294a8881ef5274576db3
Tom Brinkman <reportbase@gmail.com>
2024-01-06 19:59:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4573,18 +4573,18 @@ var taplst =
                 return;
 			var id = document.getElementById("gallery-patch-id");
             var title = document.getElementById("gallery-patch-title");
-            var root = document.getElementById("gallery-patch-root");
+            var prefix = document.getElementById("gallery-patch-prefix");
             var json = document.getElementById("gallery-patch-json");
             id.value = gallery.id;
             title.value = gallery.title;
-            root.value = gallery.root;
+            prefix.value = gallery.prefix;
             json.value = gallery.json;
             showdialog("gallery-patch", function(image)
             {
                 const form = new FormData();
                 form.append('id', id.value);
 		        form.append('title', title.value);
-                root.append('root', root.value);
+                prefix.append('prefix', prefix.value);
                 form.append('json', json.value);
                 fetch(`https://gullery.reportbase5836.workers.dev`,
                 {
@@ -4595,7 +4595,7 @@ var taplst =
                 {
                     gallery.title = title.value;
                     gallery.json = json.value;
-                    gallery.root = root.value;
+                    gallery.prefix = prefix.prefix;
                 })
             })
         }
@@ -4648,17 +4648,17 @@ var taplst =
         {
             var id = document.getElementById("gallery-add-id");
             var title = document.getElementById("gallery-add-title");
-            var root = document.getElementById("gallery-add-root");
+            var prefix = document.getElementById("gallery-add-prefix");
             var json = document.getElementById("gallery-add-json");
             id.value = Math.floor(Date.now() / 1000).toString(36);
             title.value = "";
             json.value = "";
-            root.value = "";
+            prefix.value = "";
             showdialog("gallery-add", function(image)
             {
                 const form = new FormData();
                 form.append('title', title.value);
-                form.append('root', root.value);
+                form.append('prefix', prefix.value);
                 form.append('json', json.value);
                 form.append('gallery_id', id.value);
 		        form.append('user_id', login.id);
@@ -4675,7 +4675,7 @@ var taplst =
                     k = Object.assign(k, j);
                     k.title = obj.title;
                     k.json = obj.json;
-                    k.root = obj.root;
+                    k.prefix = obj.prefix;
                     k.id = obj.gallery_id;   
                     var e = _2cnv.sliceobj.data.unshift(k);
                     _2cnv.sliceobj.set(0);
