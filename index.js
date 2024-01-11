bcfd19a2edb609b075ca4b26cf2066e1d109d0e8
Tom Brinkman <reportbase@gmail.com>
2024-01-11 15:14:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4972,7 +4972,7 @@ var buttonlst =
             var lst = [];
             lst.push(slice.name);
             lst.push(`${time+1} of ${galleryobj.length()}`)
-            var g = lst.length*40;
+            var g = Math.min(buttonobj.value()-20*4,lst.length*40);
             var a = new panel.shrink(new panel.layers(
             [
                 new panel.rounded("rgba(100,100,100)", 0, 0, 20, 20),    
