2bc8c995d4d9d7e00ec64c0520e1310baede3db5
Tom Brinkman <reportbase@gmail.com>
2024-01-17 12:57:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5054,8 +5054,8 @@ menuobj.draw = function()
                 var k = canvas.normal.findIndex(function(a){return a == e});
     		    if (k >= 0)
     			    continue;
-    		    var n = canvas.normal[e];
-    			var slice = slices[n];
+    		    var b = canvas.normal[e];
+    			var slice = slices[b];
     		    delete slice.thumbimg
     		    delete slice.thumbfitted;
     		}
